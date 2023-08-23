4098cb6860f4077e21fc8136742d0c03de1d2a3f
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-08-22 18:03:04 -0700

    Update MrKaehms.java

--- MrKaehms.java
+++ MrKaehms.java
@@ -8,9 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MrKaehms extends Teacher //implements JustForFun
 {
-    
 
-   
     
     public MrKaehms () {
 
