617c2771e11d2a064f2ba12756e595635525e428
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-08-23 11:07:40 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 import java.util.ArrayList;
 /**
  * Write a description of class Student here.
