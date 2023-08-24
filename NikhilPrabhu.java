0b16628be637599509731bd696276abe529f1c14
Nikhil Prabhu <82732097+Trolleroof@users.noreply.github.com>
2023-08-24 13:59:35 -0700

    hi

---
+++ NikhilPrabhu.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class NikhilPrabhu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NikhilPrabhu extends Student
+{
+    /**
+     * Act - do whatever the NikhilPrabhu wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
