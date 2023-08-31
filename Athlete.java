2ffaeefd39c4be168d4fd678029f50166d4c5cd4
sudhavsmsc <sudhavs@gmail.com>
2023-08-30 18:28:48 -0700

    adding Athlete.java plus all of shivani's files to repo

---
+++ Athlete.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of interface StudentAthlete here.
+ * 
+ * @author Shivani Sridhar 
+ * @version August 29, 2023
+ * Shivani, Mohith, Samika, Eric
+ */
+public interface Athlete 
+{
+    // method signatures - implement the signature below in your own class. Make sure to
+    //                     match the parameter list and return type
+    
+    public void mySport(String mySentence, int numberOfSports);
+}
