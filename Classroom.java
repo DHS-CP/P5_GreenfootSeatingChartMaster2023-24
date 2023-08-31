54e06534515e31552a1536e90234acfc12cbb874
Jonathan Adrian <jonathanadrian@Jonathans-MacBook-Pro.local>
2023-08-30 21:14:17 -0700

    added my animation and voiceover

--- Classroom.java
+++ Classroom.java
@@ -8,7 +8,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-
+//import java.util.List;
+//import java.util.Arrays;
  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  
 /**
@@ -102,7 +103,7 @@ public class Classroom extends World
         StudentDesk studentdesk32 = new StudentDesk();
         addObject(studentdesk32,6,10);
         TeacherDesk teacherdesk = new TeacherDesk();
-        addObject(teacherdesk,2,9);
+        addObject(teacherdesk,2,10);
         studentdesk31.setDeskGroup(1);
         studentdesk29.setDeskGroup(1);
         studentdesk30.setDeskGroup(1);
@@ -151,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        JonathanAdrian jonathanadrian = new JonathanAdrian();
+        addObject(jonathanadrian, 9, 3);
+        jonathanadrian.assignSeat();
   
     }
     
